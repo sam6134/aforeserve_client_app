f94a47bbad5f5f66578ee050022fbfff7193835a
bramhesh <bramhesh.srivastav@algo8.ai>
2020-07-21 14:10:50 +0530

    diskclean up file modified

--- diskCleanup.py
+++ diskCleanup.py
@@ -3,7 +3,7 @@ import subprocess
 def startCleanup():
 
     p = subprocess.Popen([r"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe","-ExecutionPolicy",
-                          "Unrestricted","/static/Start-Cleanup.ps1"])
+                          "Unrestricted","Start-Cleanup.ps1"])
     #,stdout=sys.stdout
     #print(p.communicate())
     
